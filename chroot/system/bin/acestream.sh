ab6b7fa3d8b4122098aa28dd5ed45d7479c29325
Vasiliy Solovey <miltador@yandex.ua>
2016-12-22 22:09:12 +0200

    Revert "fix(chroot): restructure to linux style"
    This reverts commit df0355874a9ccbb2345bcc48d5cd56069e4cc55d.

---
+++ chroot/system/bin/acestream.sh
@@ -0,0 +1,9 @@
+#!/system/bin/sh
+
+export ANDROID_ROOT=/storage/emulated/0
+export PYTHONHOME=/system/data/data/org.acestream.media/files/python
+export PYTHONPATH=/system/data/data/org.acestream.media/files/python/lib/python2.7/lib-dynload:/system/data/data/org.acestream.media/files/python/lib/python2.7
+export PATH=$PYTHONHOME/bin:$PATH
+export LD_LIBRARY_PATH=/system/data/data/org.acestream.media/files/python/lib:/system/data/data/org.acestream.media/files/python/lib/python2.7/lib-dynload
+cd /system/data/data/org.acestream.media/files/
+/system/data/data/org.acestream.media/files/python/bin/python /system/data/data/org.acestream.media/files/main.py "$@"
--- chroot/usr/bin/acestream.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export ANDROID_ROOT=/storage/emulated/0
-export PYTHONHOME=/data/data/org.acestream.media/files/python
-export PYTHONPATH=/data/data/org.acestream.media/files/python/lib/python2.7/lib-dynload:/data/data/org.acestream.media/files/python/lib/python2.7
-export PATH=$PYTHONHOME/bin:$PATH
-export LD_LIBRARY_PATH=/data/data/org.acestream.media/files/python/lib:/data/data/org.acestream.media/files/python/lib/python2.7/lib-dynload
-cd /data/data/org.acestream.media/files/
-/data/data/org.acestream.media/files/python/bin/python /data/data/org.acestream.media/files/main.py "$@"
